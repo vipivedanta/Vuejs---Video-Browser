b1f5e0e03bd6ed7805ba27f7a678aa3f5fe2c572
Vipin <vipins110@gmail.com>
2019-01-31 12:25:21 +0530

    Implemented youtube api , created videoList & videoListItem components and integrated with communication between them

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
 <div>
-<div>Video Browser</div>
-<SearchBar @termChange="onTermChange"></SearchBar>
+    <h3>Video Browser</h3>
+    <div class="row">
+        
+    </div>
+    <div class="row">
+        <div class="col-6"><SearchBar @termChange="onTermChange"></SearchBar></div>
+        <div class="col-6"><VideoList :videos="videos"></VideoList></div>
+    </div>
 </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import axios from 'axios';
+
 const YOUTUBE_DATA_API_KEY = 'AIzaSyCspE3SO1up8-jBCj5dbKHzxfRgM3tR-JA';
 
 export default{
     name : 'App',
     components : {
-        SearchBar : SearchBar
+        SearchBar : SearchBar,
+        VideoList : VideoList
+    },
+    data(){
+        return {
+            videos : []
+        }
     },
     methods : {
         onTermChange(searchTerm){
-            console.log(searchTerm);
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params : {
+                    key : YOUTUBE_DATA_API_KEY,
+                    type : 'video',
+                    part : 'snippet',
+                    q : searchTerm
+                }
+            }).then(response => {
+                this.videos = response.data.items;
+            });
         }
     }
 }
