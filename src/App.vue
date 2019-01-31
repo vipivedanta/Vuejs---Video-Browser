e6cc2b8e2c78d680feb2985ec689dc74c7ce5b42
Vipin <vipins110@gmail.com>
2019-01-31 10:50:44 +0530

    created youtube data api credential and declared in app component

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 
 <script>
 import SearchBar from './components/SearchBar';
+const YOUTUBE_DATA_API_KEY = 'AIzaSyCspE3SO1up8-jBCj5dbKHzxfRgM3tR-JA';
+
 export default{
     name : 'App',
     components : {
