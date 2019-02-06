c98e9699febfe2f0a74af6acf33258658d00a04c
Vipin <vipins110@gmail.com>
2019-02-06 11:35:39 +0530

    Playing video when a video item is selected from the list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
-<div>
-    <h3>Video Browser</h3>
-    <div class="row">
-        
+<div class="">
+    
+    <div class="mt-20">
+        <div class="col-md-6"><SearchBar @termChange="onTermChange"></SearchBar></div>        
     </div>
-    <div class="row">
-        <div class="col-6"><SearchBar @termChange="onTermChange"></SearchBar></div>
-        <div class="col-6"><VideoList :videos="videos"></VideoList></div>
+
+    <div class="mt-20">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-8"><VideoDetail :video="selectedVideo"></VideoDetail></div>
+                <div class="col-md-4"><VideoList :videos="videos" @onVideoSelect="videoSelect"></VideoList></div>
+            </div>
+        </div>
     </div>
+
 </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 import axios from 'axios';
 
 const YOUTUBE_DATA_API_KEY = 'AIzaSyCspE3SO1up8-jBCj5dbKHzxfRgM3tR-JA';
@@ -22,11 +29,13 @@ export default{
     name : 'App',
     components : {
         SearchBar : SearchBar,
-        VideoList : VideoList
+        VideoList : VideoList,
+        VideoDetail : VideoDetail
     },
     data(){
         return {
-            videos : []
+            videos : [],
+            selectedVideo : ''
         }
     },
     methods : {
@@ -41,7 +50,16 @@ export default{
             }).then(response => {
                 this.videos = response.data.items;
             });
+        },
+        videoSelect(video){
+            this.selectedVideo = video;
         }
     }
 }
 </script>
+
+<style>
+.mt-20{
+    margin-top:20px;
+}
+</style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="col-12">
-    <input type="text" @input="onInput" class="form-control" placeholder="Search videos here..." />
+    <div class="col-md-12">
+    <div class="row">
+        <div class="col-md-9">
+            <input type="text" v-model="keyword" class="form-control" placeholder="Search videos here..." />
+        </div>
+        <div class="col-md-2">
+            <input type="button" value="Search" class="btn btn-info" @click="searchNow"/>
+        </div>
     </div>
+    </div>   
 </template>
 
 <script>
     export default{
         name : 'SearchBar',
+        data(){
+            return {
+                keyword : ''
+            }
+        },
         methods : {
             onInput(e){
                 this.$emit('termChange',e.target.value);
+            },
+            searchNow(){
+                if(this.keyword == ''){
+                    alert('Please type a keyword to search');
+                    return;
+                }
+                this.$emit('termChange',this.keyword);
             }
         }
     }
